f852421075356e5655ca3acceca0d046b35ba11f
Jonatas Eleoteio <jonatas.eleoterio@outlook.com>
2020-07-05 01:55:27 -0300

    cadastro de usuários - recebendo e validando dados

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,20 @@
 module.exports.controllerCadastro = (app, req, res) => {
-    res.render('cadastro')
+    res.render('cadastro', {validacao: {}, dadosForm: {}})
+}
+
+module.exports.controllerCadastrar = (app,req,res) => {
+    var dadosForm = req.body
+
+    req.assert('nome', 'Nome não pode ser vazio.').notEmpty()
+    req.assert('usuario', 'Usuário não pode ser vazio.').notEmpty()
+    req.assert('senha', 'Senha não pode ser vazio.').notEmpty()
+    req.assert('casa', 'Casa não pode ser vazio.').notEmpty()
+
+    var erros = req.validationErrors()
+
+    if(erros){
+        res.render('cadastro', {validacao: erros, dadosForm: dadosForm})
+        return
+    }
+    res.send('Podemos cadastrar')
 }
--- app/routes/cadastro.js
+++ app/routes/cadastro.js
@@ -1,5 +1,11 @@
-module.exports = function(app) {
+module.exports = (app) => {
     app.get('/cadastro' ,(req, res) => {
         app.app.controllers.cadastro.controllerCadastro(app,req,res)
     })
+
+    app.post('/cadastrar', (req,res) => {
+        app.app.controllers.cadastro.controllerCadastrar(app,req,res)
+    })
 }
+
+
