fd6f6579d712857d20281188e66de91ce5022e6c
Jonatas Eleoteio <jonatas.eleoterio@outlook.com>
2020-07-07 23:59:06 -0300

    criando controller e autenticação

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,20 @@
 module.exports.controllerIndex = (app, req,res) => {
-    res.render('index')
+    res.render('index', {validacao: {}})
 } 
+
+module.exports.controllerAutenticar = (app,req,res) => {
+    const dadosForm = req.body
+
+    req.assert('usuario', 'Usuário não deve ser vazio').notEmpty()
+    req.assert('senha', 'Senha não deve ser vazia').notEmpty()
+
+    const erros = req.validationErrors()
+
+    if(erros) {
+        res.render('index', {validacao: erros})
+        console.log(erros)
+        return
+    }
+
+    res.send('tudo ok para criar a sessão')
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,4 +2,8 @@ module.exports = (app)=>{
 	app.get('/', (req, res)=>{
 		app.app.controllers.index.controllerIndex(app,req,res)
 	});
+
+	app.post('/autenticar', (req,res) => {
+		app.app.controllers.index.controllerAutenticar(app,req,res)
+	})
 }
